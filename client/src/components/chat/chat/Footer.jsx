55f81ede7ec2d674e21d58bd39e393a417b8b4ad
Saurav Jain <sauravjain@navneettoptech.com>
2024-01-06 00:32:29 +0530

    added initial commit

---
+++ client/src/components/chat/chat/Footer.jsx
@@ -0,0 +1,109 @@
+import { useEffect } from 'react';
+
+import { EmojiEmotions, AttachFile, Mic } from '@mui/icons-material';
+import { Box, styled, InputBase,Typography } from '@mui/material';
+
+import { uploadFile } from '../../../service/api';
+import {formatDate} from '../../../utils/common-utils'
+const Container = styled(Box)`
+    height: 55px;
+    background: #ededed;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    padding: 0 15px;
+    &  > * {
+        margin: 5px;
+        color: #919191;
+    }
+`;
+
+const Search = styled(Box)`
+    border-radius: 18px;
+    background-color: #FFFFFF;
+    width: calc(94% - 100px);
+`;
+
+const InputField = styled(InputBase)`
+    width: 100%;
+    padding: 20px;
+    padding-left: 25px;
+    font-size: 14px;
+    height: 20px;
+    width: 100%;
+`;
+
+const ClipIcon = styled(AttachFile)`
+    transform: 'rotate(45deg)'
+`;
+const Text = styled(Typography)`
+    font-size: 14px;
+    padding: 0 25px 0 5px;
+`;
+
+const Time = styled(Typography)`
+    font-size: 10px;
+    color: #919191;
+    margin-top: 6px;
+    word-break: keep-all;
+    margin-top: auto;
+`;
+export const TextMessage = ({ message }) => {
+    
+    return (
+        <>
+            <Text>{message.text}</Text>
+            <Time>{formatDate(message.createdAt)}</Time>
+        </>
+    )
+}
+
+export const Footer = ({ sendText, value, setValue, setFile, file, setImage }) => {
+
+    useEffect(() => {
+        const getImage = async () => {
+            if (file) {
+                const data = new FormData();
+                data.append("name", file.name);
+                data.append("file", file);
+
+                const response = await uploadFile(data);
+                setImage(response.data);
+            }
+        }
+        getImage();
+    }, [file])
+
+    const onFileChange = (e) => {
+        console.log("58",e)
+        setValue(e.target.files[0].name);
+        setFile(e.target.files[0]);
+    }
+  
+    return (
+        <Container>
+            <EmojiEmotions />
+            <label htmlFor="fileInput">
+                <ClipIcon />
+            </label>
+            <input
+                type='file'
+                id="fileInput"
+                style={{ display: 'none' }}
+                onChange={(e) => onFileChange(e)}
+            />
+
+            <Search>
+                <InputField
+                    placeholder="Type a message"
+                    inputProps={{ 'aria-label': 'search' }}
+                    onChange={(e) => setValue(e.target.value)}
+                    onKeyPress={(e) => sendText(e)}
+                    value={value}
+                />
+            </Search>
+            <Mic />
+        </Container>
+    )
+}
+
---
+++ server/controller/conversation-controller.js
@@ -0,0 +1,40 @@
+import Conversation  from "../modal/Conversation.js";
+
+
+export const newConversation = async (request, response) => {
+    let senderId = request.body.senderId;
+    let receiverId = request.body.receiverId;
+
+    const exist = await Conversation.findOne({ members: { $all: [receiverId, senderId]  }})
+    
+    if(exist) {
+        response.status(200).json('conversation already exists');
+        return;
+    }
+    const newConversation = new Conversation({
+        members: [senderId, receiverId]
+    });
+
+    try {
+        const savedConversation = await newConversation.save();
+        response.status(200).json(savedConversation);
+    } catch (error) {
+        response.status(500).json(error);
+    }
+
+}
+
+export const getConversation = async (request, response) => {
+    try {
+        const conversation = await Conversation.findOne({ members: { $all: [ request.body.senderId, request.body.receiverId] }});
+        // const conv1=await findById({members:{$all:[request.body.senderId,request.body.receiverId]}})
+        response.status(200).json(conversation);
+    } catch (error) {
+        response.status(500).json(error);
+    }
+
+}
+
+
+// note-: newConversation is the mongoose documnent with the _id and with the field members
+// in documnent only memebers and _id
